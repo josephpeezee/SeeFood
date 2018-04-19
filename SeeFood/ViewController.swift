bce710c7a394b56753e1c4085c3f7bebc43cc8a2
Joseph Pizzo <jp@norsecorp.com>
2018-04-18 20:37:08 -0400

    getting camera working with UIImagePickerControllerDelegate and UINavigationControllerDelegate, setting the plist to ask for access to photo library and camera, works and takes photos. mError after running on simulator --- 'Source type 1 not available' -- once ran on iPhone SE it worked, but image was distorted

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,18 +7,55 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+// Addes in UIPickerControllerDelegate and UINavigationControllerDelegate to enable use of camera
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    // Image View
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //any error may be tied to the delegate set in ViewController class
+        imagePicker.delegate = self
+        
+        // brings up an UIImagePicker that contains the camera module allowing the user to use the front or rear camera
+        imagePicker.sourceType = .camera
+        
+        // brings up an UIImagePicker that contains the camera module allowing the user to use the photolibrary
+        //imagePicker.sourceType = .photoLibrary
+        
+        // Is the user allowed to edit the image? allows the user to crop image to decrease area of item
+        imagePicker.allowsEditing = false
+    }
+    
+    // delegate method- tells the Viewcontroller class that the user haas picked an image or a movie
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        
+        //down cast to UIImage with optional binding with as? and set as UIImage
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Camera Button Tapped
+    @IBAction func CameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
     }
+    
 
 
 }
